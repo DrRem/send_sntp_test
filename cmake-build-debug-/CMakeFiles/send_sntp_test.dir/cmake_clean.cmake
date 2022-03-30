273d75b99c0cca5ca8d29726af8f0d46bd620c85
DrRem <fffeifei151@gmail.com>
2022-03-30 16:07:04 +0800

    commit

---
+++ cmake-build-debug-/CMakeFiles/send_sntp_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/send_sntp_test.dir/main.c.o"
+  "send_sntp_test.pdb"
+  "send_sntp_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/send_sntp_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
